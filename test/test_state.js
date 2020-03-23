34364eadd79b194b7cf2b4691c7fa101f9ec0338
Philipp Hagemeister <philipp.hagemeister@boxine.de>
2020-03-23 17:35:09 +0100

    Fix tests
    I overlooked this change in the tests between PRs.
    Add a gitlab CI configuration file so that the tests are executed automatically.

--- test/test_state.js
+++ test/test_state.js
@@ -9,6 +9,7 @@ describe('state', () => {
             foo: 1,
         }, 1580000000001);
         assert.deepStrictEqual(state.runs, [{
+            knownVersions: [],
             firstFinished: 1580000000001,
             lastFinished: 1580000000001,
             results: {foo: 1},
@@ -18,6 +19,7 @@ describe('state', () => {
             foo: 1,
         }, 1580000000002);
         assert.deepStrictEqual(state.runs, [{
+            knownVersions: [],
             firstFinished: 1580000000001,
             lastFinished: 1580000000002,
             results: {foo: 1},
@@ -27,10 +29,12 @@ describe('state', () => {
             foo: 2,
         }, 1580000000003);
         assert.deepStrictEqual(state.runs, [{
+            knownVersions: [],
             firstFinished: 1580000000001,
             lastFinished: 1580000000002,
             results: {foo: 1},
         }, {
+            knownVersions: [],
             firstFinished: 1580000000003,
             lastFinished: 1580000000003,
             results: {foo: 2},
@@ -40,14 +44,17 @@ describe('state', () => {
             foo: 1,
         }, 1580000000004);
         assert.deepStrictEqual(state.runs, [{
+            knownVersions: [],
             firstFinished: 1580000000001,
             lastFinished: 1580000000002,
             results: {foo: 1},
         }, {
+            knownVersions: [],
             firstFinished: 1580000000003,
             lastFinished: 1580000000003,
             results: {foo: 2},
         }, {
+            knownVersions: [],
             firstFinished: 1580000000004,
             lastFinished: 1580000000004,
             results: {foo: 1},
